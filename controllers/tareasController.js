2b2150ffb5c6d21e1c7466eb410ec8b781f2f85d
Sebas404040 <delgadopalitogarbage@gmail.com>
2025-09-19 20:54:46 -0500

    feat: :sparkles: logical implemency
    logic in tareasController file and printings in notificador file
    
    none

--- controllers/tareasController.js
+++ controllers/tareasController.js
@@ -1,6 +1,7 @@
 import Tarea from '../models/Tarea.js';
 import database from '../config/database.js'; 
 import notificador from '../utils/notificador.js';
+import inquirer from 'inquirer';
 
 class GestorTareas {
   async agregarTarea(descripcion) {
@@ -19,11 +20,75 @@ class GestorTareas {
     try {
       const collection = await database.getCollection('tareas');
       const tareas = await collection.find({}).toArray();
-      notificador.mostrarTareas(tareas);
+      
+      const eleccionEstado = await notificador.elegirEstado(); 
+
+      let tareasFiltradas = [];
+      if (eleccionEstado === "completadas") {
+        tareasFiltradas = tareas.filter(tarea => tarea.completada === true);
+      } else if (eleccionEstado === "No completadas") {
+        tareasFiltradas = tareas.filter(tarea => tarea.completada === false);
+      } else {
+        tareasFiltradas = tareas; 
+      }
+      notificador.mostrarTareas(tareasFiltradas);
     } catch (error) {
       notificador.error('Error al cargar tareas:', error);
     }
   }
+
+  async marcarTareaCompleta() {
+    try {
+      const collection = await database.getCollection("tareas");
+      const tareas = await collection.find({}).toArray();
+
+      const tareasIdsParaCompletar = await notificador.elegirTareasParaCompletar(tareas);
+
+      if (tareasIdsParaCompletar.length === 0) {
+        return; // No hay tareas seleccionadas o no hay pendientes
+      }
+
+      const resultado = await collection.updateMany(
+        { _id: { $in: tareasIdsParaCompletar } },
+        { $set: { completada: true } }
+      );
+
+      if (resultado.modifiedCount > 0) {
+        notificador.tareaMarcadaCompleta();
+      }
+    } catch (error) {
+      notificador.error('Error al marcar tareas como completadas:', error);
+    }
+  }
+
+  async eliminarTarea() {
+    try {
+      const collection = await database.getCollection("tareas");
+      const tareas = await collection.find({}).toArray();
+
+      if (tareas.length === 0) {
+        return notificador.info('No hay tareas para eliminar.');
+      }
+
+      notificador.mostrarTareas(tareas);
+      const tareaIdParaEliminar = await notificador.elegirTarea(tareas);
+
+      if (!tareaIdParaEliminar) {
+        return notificador.eliminacionCancelada();
+      }
+
+      const resultado = await collection.deleteOne({ _id: tareaIdParaEliminar });
+
+      if (resultado.deletedCount === 1) {
+        notificador.tareaEliminada();
+      } else {
+        notificador.error('No se pudo encontrar la tarea para eliminar.');
+      }
+    } catch (error) {
+      notificador.error('Error al eliminar la tarea:', error);
+    }
+  }
+
 }
 
 export default GestorTareas;
--- utils/notificador.js
+++ utils/notificador.js
@@ -1,4 +1,5 @@
 import chalk from 'chalk';
+import inquirer from 'inquirer';
 
 class Notificador {
   
@@ -18,13 +19,27 @@ class Notificador {
     console.log(chalk.yellow('👋 ¡Hasta pronto!'));
   }
 
+  async elegirEstado() {
+    const { eleccion } = await inquirer.prompt([
+        {
+            type: "list",
+            name: "eleccion",
+            message: "Elija tarea por tipo de estado",
+            choices: [
+                "completadas",
+                "No completadas"
+            ]
+        }
+    ])
+    return eleccion
+  }
+  
   mostrarTareas(tareas) {
     console.log(chalk.cyan('--- Lista de Tareas ---'));
     if (tareas.length === 0) {
       this.info('No hay tareas para mostrar.');
       return;
     }
-
     tareas.forEach((tarea, index) => {
       const estado = tarea.completada 
         ? chalk.green('Completada') 
@@ -32,6 +47,65 @@ class Notificador {
       console.log(`${chalk.bold(index + 1)}. [${estado}] - ${tarea.descripcion}`);
     });
     console.log(chalk.cyan('-----------------------'));
+  }
+
+  tareaMarcadaCompleta() {
+    console.log(chalk.green('✔ Tarea marcada como completada.'));
+  }
+
+  tareaEliminada() {
+    console.log(chalk.green('✔ Tarea eliminada exitosamente.'));
+  }
+
+  eliminacionCancelada() {
+    this.info('La eliminación ha sido cancelada.');
+  }
+
+  async elegirTareasParaCompletar(tareas) {
+    const tareasPendientes = tareas.filter(t => !t.completada);
+
+    if (tareasPendientes.length === 0) {
+      this.info('¡Felicidades! No hay tareas pendientes para marcar.');
+      return [];
+    }
+
+    const choices = tareasPendientes.map((tarea, index) => ({
+      value: tarea._id,
+      name: `${index + 1}. ${tarea.descripcion}`
+    }));
+
+    const { tareasIds } = await inquirer.prompt([
+      { type: 'checkbox', name: 'tareasIds', message: 'Seleccione las tareas a completar:', choices }
+    ]);
+
+    return tareasIds;
+  }
+
+  async elegirTarea(tareas) {
+    const choices = tareas.map((tarea, index) => ({
+      value: tarea._id,
+      name: `${index + 1}. [${tarea.completada ? 'Completada' : 'Pendiente'}] - ${tarea.descripcion}`
+    }));
+
+    const { tareaId } = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'tareaId',
+        message: 'Seleccione la tarea a eliminar:',
+        choices
+      }
+    ]);
+
+    const { confirmacion } = await inquirer.prompt([
+      {
+        type: 'confirm',
+        name: 'confirmacion',
+        message: chalk.yellow(`¿Está seguro de que desea eliminar esta tarea?`),
+        default: false
+      }
+    ]);
+
+    return confirmacion ? tareaId : null;
   }
 }
 
